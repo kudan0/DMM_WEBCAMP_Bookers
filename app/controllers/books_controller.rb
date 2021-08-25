293fe391dd3285c87e723eadc98ed67161a47be4
kudan0 <7xxx.ccs@gmail.com>
2021-08-25 08:08:05 +0000

    [add]一覧表示

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,15 +1,25 @@
 class BooksController < ApplicationController
   def index
+    @new_book = Book.new
+    @books = Book.all
   end
 
   def new
-    @new_book = Book.new
 
   end
 
   def create
+    @book = Book.new(book_params)
+    @book.save
+    redirect_to '/books'
   end
 
   def edit
   end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
 end
